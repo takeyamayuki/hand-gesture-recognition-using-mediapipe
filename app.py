fe9db420fe1ca8dc884a643f61d04cb048894dd1
takeyamayuki <namiki.takeyama@gmail.com>
2023-04-19 19:11:38 +0900

    add Dondake gesture model

--- app.py
+++ app.py
@@ -161,6 +161,7 @@ def main():
                 # 描画
                 debug_image = draw_bounding_rect(use_brect, debug_image, brect)
                 debug_image = draw_landmarks(debug_image, landmark_list)
+                print(most_common_fg_id)
                 debug_image = draw_info_text(
                     debug_image,
                     brect,
