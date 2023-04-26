d9aef869000eecaf26daf572327ebfb7c039ae84
takeyamayuki <namiki.takeyama@gmail.com>
2023-04-26 13:35:33 +0900

    changed input type

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ from model import PointHistoryClassifier
 def get_args():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--device", type=int, default=0)
+    parser.add_argument("--device", type=str, default=0)
     parser.add_argument("--width", help='cap width', type=int, default=960)
     parser.add_argument("--height", help='cap height', type=int, default=540)
 
